a3ce05152a64c9c53d0b81e124356f08af7bf3fe
BibhushanKarki <bibhushan.karki11@gmail.com>
2022-03-27 22:08:25 +0545

    destructuring and prop shorthand

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,50 +1,19 @@
-const request = require('request');
 const geocode =require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-// const url = 'http://api.weatherstack.com/current?access_key=b75f87f480ce164e2f4e3ddd3fac72af&query=26.452475,87.271782&units=f';
-
-// request({url:url}, (error,response) => {
-//   if (error) {
-//     console.log('Unable to connect to weather service!');
-//   } else if(response.body){
-//     console.log('Unable to find location');
-//   }
-//   else {
-//     const data= JSON.parse(response.body);
-//     console.log(`It is currently ${data.current.temperature} Fahrenheit out. It feels like ${data.current.feelslike} Fahrenheit.`);
-//   }
-// });
-
-// Geocoding
-
-// const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmliaHVzaGFuIiwiYSI6ImNsMTk1NTdnODBwbmszanJpaHVkYWt2MzUifQ.QDLTJpqZ_IS7KZE0uzP8RQ';
-
-// request({url:geoCodeUrl,json:true}, (error,response) => {
-//   if (error) {
-//       console.log('Unable to connect to location service!');
-//   } else if(response.body.features.length === 0){
-//     console.log('Unable to find location. Try another search.');
-//   }
-//   else{
-//   const latitude= response.body.features[0].center[1];
-//   const longitute= response.body.features[0].center[0];
-//   console.log(latitude,longitute);
-//   }
-// });
 const address = process.argv[2];
 if(!address){
   console.log('Please provide an address!');
 }else{
-  geocode(address,(error,data)=>{
+  geocode(address,(error,{latitude,longitude,location} = {})=>{
     if(error){
       return console.log(error);
     }
-    forecast(data.latitude,data.longitude,(error,forecastData)=>{
+    forecast(latitude,longitude,(error,forecastData)=>{
       if(error){
         return console.log(error);
       }
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     })
   })
