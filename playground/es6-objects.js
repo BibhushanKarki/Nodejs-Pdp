251b5769f857c085e7d0e155eb71d397459c8eee
BibhushanKarki <bibhushan.karki11@gmail.com>
2022-03-27 21:57:13 +0545

    es6-objects

---
+++ playground/es6-objects.js
@@ -0,0 +1,36 @@
+// Object property shorthand
+
+const { log } = require("console");
+
+const name = "Bibhushan"
+const useAge = 24
+
+const user={
+  name,
+  age:useAge,
+  location:'Biratnagar'
+}
+console.log(user);
+
+// Object destructuring
+
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+  rating:4.2
+};
+
+// const label = product.label;
+// const stock = product.stock;
+
+// const { label:productLabel , stock, rating = 5 } = product;
+// console.log(productLabel,stock);
+// console.log(rating);
+
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock);
+}
+
+transaction('order', product);
