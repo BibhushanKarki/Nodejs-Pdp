7ba2b598aa61567370fe338c567cab3054cfd4aa
BibhushanKarki <bibhushan.karki11@gmail.com>
2022-03-27 21:45:02 +0545

    callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -32,15 +32,22 @@ const forecast = require('./utils/forecast');
 //   console.log(latitude,longitute);
 //   }
 // });
+const address = process.argv[2];
+if(!address){
+  console.log('Please provide an address!');
+}else{
+  geocode(address,(error,data)=>{
+    if(error){
+      return console.log(error);
+    }
+    forecast(data.latitude,data.longitude,(error,forecastData)=>{
+      if(error){
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    })
+  })
+}
 
-geocode('Biratnagar',(error,data)=>{
-  console.log('Error',error);
-  console.log('Data',data);
-})
 
-// Forecast
-
-forecast(-75.7088,44.1545,(error,data)=>{
-  console.log('Error',error);
-  console.log('Data',data);
-})
