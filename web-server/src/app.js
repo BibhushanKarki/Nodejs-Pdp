ef680c7b0479bc0dcc80355fcbcec300b47acb0f
BibhushanKarki <bibhushan.karki11@gmail.com>
2022-04-03 14:37:13 +0545

    api,query string,json http endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 const app = express();
 
 // Define paths for express config
@@ -38,12 +39,52 @@ app.get('/help', (req,res)=>{
   });
 })
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'It is snowing',
-    location: 'Philadelphia'
-  });
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide an address'
+    })
+  }
+  geocode(req.query.search, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.search
+      })
+    })
+  })
 })
 
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+    res.send({
+    products: [],
+  });
+})
+app.get('/help/*',(req,res)=>{
+  res.render('404',{
+    title:'404',
+    name:'Bibhushan',
+    errorMessage:'Help article not found.'
+  });
+})
+app.get('*', (req, res) => {
+  res.render('404', {
+    'title':'404',
+    'name':'Bibhushan',
+    'errorMessage': 'Page not found.',
+  });
+})
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 })
